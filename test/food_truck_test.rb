79f42c53e9ae6367e2af4802e865fb698176f610
DougWelchons <doug.welchons@gmail.com>
2021-01-12 13:49:19 -0700

    Add food truck test for sell_item?

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -29,4 +29,29 @@ class FoodTruckTest < Minitest::Test
     assert_equal 55, food_truck.check_stock(item1)
     assert_equal expected, food_truck.inventory
   end
+
+  def test_it_can_ceck_if_it_sells_an_item
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+
+    assert_equal true, food_truck.sell_item?(item1)
+    assert_equal false, food_truck.sell_item?(item2)
+  end
 end
+
+# pry(main)> event.food_trucks_that_sell(item4)
+# #=> [#<FoodTruck:0x00007fe1349bed40...>]
+#
+# pry(main)> food_truck1.potential_revenue
+# #=> 148.75
+#
+# pry(main)> food_truck2.potential_revenue
+# #=> 345.00
+#
+# pry(main)> food_truck3.potential_revenue
+# #=> 243.75
