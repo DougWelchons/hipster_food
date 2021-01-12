881318570e73305229f5605a31f5e557b6cc6971
DougWelchons <doug.welchons@gmail.com>
2021-01-12 13:45:04 -0700

    Add event class tests

---
+++ test/event_test.rb
@@ -0,0 +1,66 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/event'
+require './lib/food_truck'
+require './lib/item'
+
+class EventTest < Minitest::Test
+  def test_it_exists_and_has_readable_attributes
+    event = Event.new("South Pearl Street Farmers Market")
+
+    assert_instance_of Event, event
+    assert_equal "South Pearl Street Farmers Market", event.name
+    assert_equal [], event.food_trucks
+  end
+
+  def test_it_can_add_food_trucs
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+    event.add_food_truck(food_truck1)
+
+    assert_equal [food_truck1], event.food_trucks
+  end
+
+  def test_it_can_return_food_truck_names
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    # item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    # item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    # item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    # item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    # food_truck1.stock(item1, 35)
+    # food_truck1.stock(item2, 7)
+    # food_truck2.stock(item4, 50)
+    # food_truck2.stock(item3, 25)
+    # food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], event.food_truck_names
+  end
+
+  def test
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    assert_equal [food_truck1, food_truck3], event.food_trucks_that_sell(item1)
+  end
+end
